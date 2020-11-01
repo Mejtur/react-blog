9a8bde28a19846b028577c453df6bed1eb4cd1fc
mejtur <mejturnl@gmail.com>
2020-11-01 21:53:35 +0100

    favourites
    getting favourites from contentful

---
+++ src/components/SingleFeatured.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SingleFeatured({image,title,tags,publishDate}) {
+    return (
+        <div>
+            {title}
+        </div>
+    )
+}
+
+export default SingleFeatured
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import Navbar from '../components/Navbar';
+import Logo from '../images/logo.png';
+import {featuredClient} from '../client';
+import SingleFeatured from '../components/SingleFeatured';
 
 function Home() {
+
+    const [featured,setFeatured] = useState(null);
+
+    useEffect(()=>{
+        featuredClient.getEntries().then((response)=>{
+            setFeatured(response.items);
+        }).catch(console.error);
+    },[]);
+
+
+    console.log(featured);
     return (
-        <div>
+        <div className="home">
             <Navbar />
+            <div className="home__logoContainer">
+                <h1 className="home__title">All about travel</h1>
+                <img src={Logo} alt="logo"/>
+            </div>
+            <div className="home__featured">
+                <h1 className="home__title">Featured post</h1>
+                {
+                    featured ? featured.map((singleFeatured)=> 
+                    <SingleFeatured key={singleFeatured.fields.id} 
+                    image={singleFeatured.fields.image} 
+                    tags={singleFeatured.fields.tags} 
+                    title={singleFeatured.fields.title}
+                    publishDate={singleFeatured.fields.publishDate}/>
+                    ) : "Loading..."
+                }
+            </div>
         </div>
     )
 }
